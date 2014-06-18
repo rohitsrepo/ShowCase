9fae6929419816e9960934d8375901041225c5b7
Rohit <rgarg1992@gmail.com>
2014-06-19 02:28:27 +0530

    Added support for voting and commenting on reader and composition page.

--- ShowCase/static/js/src/controllers/composition.js
+++ ShowCase/static/js/src/controllers/composition.js
@@ -3,13 +3,33 @@ var compositionCtrlModule = angular.module('controller.composition', ['security.
 compositionCtrlModule.controller('compositionCtrl', function ($scope, securityFactory, compositionFactory, $stateParams, $location) {
     'use strict';
     
+    $scope.newComment = '';
+    
     $scope.composition = compositionFactory.manager.get({compositionId: $stateParams.compositionId}, function (data) {
         if ((data.slug).localeCompare($stateParams.slug)) {
             $location.path('compositions/' + data.id + '/' + data.slug);
         }
-        compositionFactory.votes.get($stateParams.compositionId).then(function (votes) {
-            $scope.vote = votes;
-            $scope.comments = compositionFactory.comments.query({compositionId: $stateParams.compositionId});
-        });
+        $scope.comments = compositionFactory.comments.query({compositionId: $stateParams.compositionId});
     });
+    
+    $scope.voting = function (vote) {
+        compositionFactory.votes.put($scope.composition.id, vote).then(function (res) {
+            $scope.composition.vote = res.data;
+        }, function (res) {
+            //TODO handle error according to status of error.
+            // Global exception handling.
+            console.log('Reader controller...error while putting votes', res);
+            if (res.status === 403) {
+                alert('Seems like you have already voted mate!!!');
+            }
+        });
+    };
+    
+    $scope.commenting = function () {
+        var res = compositionFactory.comments.save({compositionId: $scope.composition.id}, {comment: $scope.newComment}, function (res) {
+            //TODO - instead of reloading all comments - just append the new comment to the list - If control reaches here we already know that the request passed.
+            $scope.comments = compositionFactory.comments.query({compositionId: $scope.composition.id});
+            $scope.newComment = '';
+        });
+    };
 });
--- ShowCase/static/js/src/controllers/reader.js
+++ ShowCase/static/js/src/controllers/reader.js
@@ -3,21 +3,18 @@ var showcaseApp = angular.module('controller.reader', ['security.service', 'arti
 showcaseApp.controller('readerCtrl', function ($scope, securityFactory, compositionFactory, logger) {
     'use strict';
     
-    $scope.compositions = compositionFactory.manager.query(function (data) {
-        angular.forEach(data, function (composition, key) {
-            compositionFactory.votes.get(composition.id).then(function (vote) {
-                $scope.compositions[key].vote = vote;
-            });
-        });
-    });
+    $scope.compositions = compositionFactory.manager.query();
     
-    $scope.voting = function (composition, vote) {
-        compositionFactory.votes.put(composition.id, vote).then(function (res) {
-            $scope.vote = res.data;
+    $scope.voting = function (index, vote) {
+        compositionFactory.votes.put($scope.compositions[index].id, vote).then(function (res) {
+            $scope.compositions[index].vote = res.data;
         }, function (res) {
             //TODO handle error according to status of error.
             // Global exception handling.
             logger('Reader controller...error while putting votes', res);
+            if (res.status === 403) {
+                alert('Seems like you have already voted mate!!!');
+            }
         });
     };
 });
