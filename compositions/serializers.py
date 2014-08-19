8438824f341ec677cde913f3b1c74a44538490f0
sourins <sourin.sutradhar@gmail.com>
2014-08-19 21:57:09 +0530

    Adding modified CompositionSerializer(with TagSerializer)

--- compositions/serializers.py
+++ compositions/serializers.py
@@ -2,6 +2,21 @@ from rest_framework import serializers
 from .models import Composition
 from django.conf import settings
 from ShowCase.serializers import HyperlinkedImageField, HyperlinkedFileField, URLImageField
+from rest_framework.exceptions import ParseError
+
+
+class TagListSerializer(serializers.WritableField):
+    
+    def from_native(self, data):
+        if type(data) is not list:
+            raise ParseError("Expected a list of data")    
+        return data
+
+    def to_native(self, obj):
+        if type(obj) is not list:
+            return [tag.name for tag in obj.all()]
+        return obj
+
 
 
 class CompositionSerializer(serializers.ModelSerializer):
@@ -9,6 +24,7 @@ class CompositionSerializer(serializers.ModelSerializer):
     #display_image = HyperlinkedImageField(source='display_image', required=False, default_url=settings.DEFAULT_COMPOSITION_IMAGE_PICTURE)
     matter = URLImageField(source='matter')
     timesince = serializers.CharField(source='timesince', read_only=True)
+    tags = TagListSerializer(required=False)
 
     class Meta:
         model = Composition
