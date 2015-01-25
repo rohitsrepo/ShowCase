3b49ee40789bb224e56477ba577e026535c3e2ed
Rohit <rgarg1992@gmail.com>
2015-01-25 11:22:54 +0530

    [Beta-Cleanup]- Remove tagging completely.

--- search/views.py
+++ search/views.py
@@ -7,8 +7,6 @@ from compositions.models import Composition
 from compositions.serializers import CompositionSerializer
 from accounts.models import User
 from accounts.serializers import NewUserSerializer
-from tagging.models import Tag
-from tagging.serializers import TagSerializer
 
 # Create your views here.
 
@@ -17,16 +15,13 @@ class SearchDetail(APIView):
         query = request.DATA['query']
         users = User.objects.get(Q(first_name__contains='lily') | Q(last_name__contains='lily'))
         compositions = Composition.objects.get(title__contains = query)
-        tags = Tag.objects.get(tag_name__contains= query)
 
         user_ser = NewUserSerializer(User, many = True, context={'request': request})
         composition_ser = CompositionSerializer(Composition, many = True, context={'request': request})
-        tag_ser = TagSerializer(Tag, many = True, context={'request': request})
         
         ser_data = {
             'USER' : user_json,
-            'COMPOSITION' : composition_json,
-            'TAG' : tag_json
+            'COMPOSITION' : composition_json
             }
         Response(ser_data)
         
--- tagging/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Tag
-from django.conf import settings
-
-class TagSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Tag
-        fields = ('id', 'tag_name', 'tag_def', 'created')
