d7ab7e0b81d7aabae0f06a66b53f913e04d7918d
Rohit <rgarg1992@gmail.com>
2015-02-01 00:57:49 +0530

    Sync webclient from the other branch.

--- ShowCase/static/js/reader/custom/main.js
+++ ShowCase/static/js/reader/custom/main.js
@@ -2,5 +2,17 @@ $(document).ready(function() {
 	 $(".middle a").click(function() {
         $.scrollTo($(".second-view"), { duration: 1200});
     });
+	 $(".da0").click(function() {
+        $.scrollTo($(".feed1"), { duration: 1000});
+    });
+	 $(".da1").click(function() {
+        $.scrollTo($(".feed2"), { duration: 1000});
+    });
+	 $(".da2").click(function() {
+        $.scrollTo($(".feed3"), { duration: 1000});
+    });
+	 $(".da3").click(function() {
+        $.scrollTo($(".feed4"), { duration: 1000});
+    });
 
 });
