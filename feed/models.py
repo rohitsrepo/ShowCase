2e509300f5de238a1cf9de8b6370b55d9fa01826
sourins <sourin.sutradhar@gmail.com>
2015-02-19 04:00:59 +0530

    Adding editor list table
    Editor list objects are composed of composition and interpretation
    objects instead of just their pk values

--- feed/models.py
+++ feed/models.py
@@ -1,1 +1,8 @@
 from django.db import models
+from compositions.models import Composition
+from interpretations.models import Interpretation
+
+
+class EditorsList(models.Model):
+    composition = models.ForeignKey(Composition)
+    interpretation = models.ForeignKey(Interpretation)
