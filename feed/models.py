8ad94fe2547fac7f7295ab218a75228acfb667b5
sourins <sourin.sutradhar@gmail.com>
2015-02-19 04:49:43 +0530

    Adding temporary feed post table
    All objects of this table will be deleted after each request

--- feed/models.py
+++ feed/models.py
@@ -3,6 +3,20 @@ from compositions.models import Composition
 from interpretations.models import Interpretation
 
 
-class EditorsList(models.Model):
+def get_upload_file_name_composition(instance, filename):
+    return 'Users/%s/Compositions/%s/%s' % (instance.artist.id, instance.created, filename)
+
+
+class EditorsListPost(models.Model):
     composition = models.ForeignKey(Composition)
     interpretation = models.ForeignKey(Interpretation)
+
+    
+class TempFeedPosts(models.Model):
+    painting_image = models.FileField(upload_to=get_upload_file_name_composition)
+    painting_name = models.CharField(max_length=100, blank=False, verbose_name='Painting_name')
+    painter = models.CharField(max_length=100, blank=False, verbose_name='Painter')
+    interpretation = models.CharField(max_length=500, verbose_name='Top Interpretation')
+    interpretation_writer = models.CharField(max_length=100, blank=False, verbose_name='Interpretation Writer')
+    interpretation_votes = models.PositiveIntegerField(default=0)
+    
