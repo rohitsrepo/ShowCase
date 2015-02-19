f5209a2ed455e41e2094ae1229b891be9e5ae445
sourins <sourin.sutradhar@gmail.com>
2015-02-19 22:14:55 +0530

    Adding paginated TempFeedPosts serializer

--- feed/serializers.py
+++ feed/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import TempFeedPosts
 from ShowCase.serializers import URLImageField
+from rest_framework.pagination import PaginationSerializer
 
 class TempFeedPostsSerializer(serializers.ModelSerializer):
     painting_image = URLImageField(source='painting_image')
@@ -8,3 +9,11 @@ class TempFeedPostsSerializer(serializers.ModelSerializer):
     class Meta:
         model = TempFeedPosts
         fields = ('id', 'painting_image', 'painting_name', 'painter', 'interpretation', 'interpretation_writer', 'interpretation_votes')	
+        
+        
+
+class PaginatedTempFeedPostsSerializer(PaginationSerializer):
+    #Serializes page objects of user querysets.
+
+    class Meta:
+        object_serializer_class = TempFeedPostsSerializer
