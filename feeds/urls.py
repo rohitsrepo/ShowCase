9e932f2393bbbe4ad0502b38f073dc9b2feac51c
sourins <sourin.sutradhar@gmail.com>
2015-03-09 01:33:33 +0530

    Customising editors pick app admin portal

--- feeds/urls.py
+++ feeds/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import patterns, url, include
 from rest_framework.urlpatterns import format_suffix_patterns
-from . import views
 
 urlpatterns = patterns('',
                        url(r'^/editors$', 'feeds.views.editors_pick_list'),
