bcc51f5fe34e377164d65ca185b6d3a844e542e9
sourins <sourin.sutradhar@gmail.com>
2015-03-09 01:32:41 +0530

    Customising composition app admin portal

--- compositions/admin.py
+++ compositions/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from compositions.models import Composition
+from interpretations.models import Interpretation
 
-# Register your models here
 
-admin.site.register(Composition)
+class InterpretationInline(admin.StackedInline):
+    model = Interpretation
+    extra = 2
+
+
+class CompositionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'artist', 'slug')
+    list_filter = ['created']
+    search_fields = ['title', 'slug', 'description']
+    fields = ['title', 'artist', 'description', 'slug', 'matter']
+    inlines = [InterpretationInline]
+    
+
+admin.site.register(Composition, CompositionAdmin)
