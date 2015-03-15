3d03e1142024c1fe5dbc6ac093e8b0f512aa952b
Rohit <rgarg1992@gmail.com>
2015-03-16 00:16:05 +0530

    Fix for fade on scroll issue

--- ShowCase/static/js/components/fadeOnScroll.directive.js
+++ ShowCase/static/js/components/fadeOnScroll.directive.js
@@ -1,19 +1,18 @@
 angular.module('module.scrollTo')
 .directive('fadeOnScroll', ['$document', function ($document) {
 	return function(scope, element, attrs) {
-		if (!scope[attrs.ngHide]){
-            element.addClass('animated fadeIn');
-	        $document.bind('scroll', function () {
-	            if ($document.scrollTop() < 100) {
+        $document.bind('scroll', function () {
+            if ($document.scrollTop() < 100) {
+            	if (element.hasClass('fadeOut')){
 		            element.removeClass('animated fadeOut');
 		            element.addClass('animated fadeIn');
-		            element.show();
-	            } else {
-		            element.removeClass('animated fadeIn');
-		            element.addClass('animated fadeOut');
-		            element.hide();
-	            }
-	        });
-	   }
+		        }
+	            element.show();
+            } else {
+	            element.removeClass('animated fadeIn');
+	            element.addClass('animated fadeOut');
+	            element.hide();
+            }
+        });
 	};
 }]);
