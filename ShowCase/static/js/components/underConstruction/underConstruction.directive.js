74717fe374f1e4086a092950778d4b477c4183c1
rohitsrepo@gmail.com <rohit>
2015-03-22 15:25:22 +0530

    Adding under construction to curtains

--- ShowCase/static/js/components/underConstruction/underConstruction.directive.js
+++ ShowCase/static/js/components/underConstruction/underConstruction.directive.js
@@ -2,6 +2,7 @@ angular.module('module.underConstruction', ['ngMaterial'])
 .directive('underConstruction', ['$mdDialog', function ($mdDialog) {
     return function (scope, element, attrs) {
         element.bind('click', function (event) {
+            $('.curtain-overlay-placeholder').click();
             $mdDialog.show({
                 targetEvent: event,
                 templateUrl: '/static/js/components/underConstruction/underConstruction.tpl.html',
