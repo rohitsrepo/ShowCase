da20ab818efa12e95f7133684114e5b02964afd3
rohitsrepo@gmail.com <rohit>
2015-04-11 00:45:02 +0530

    Adding public field to interpretation admin

--- interpretations/admin.py
+++ interpretations/admin.py
@@ -6,7 +6,7 @@ class InterpretationAdmin(admin.ModelAdmin):
     list_display = ('composition', 'user', 'interpretation')
     list_filter = ['created']
     search_fields = ['interpretation']
-    fields = ['composition', 'user', 'interpretation']
+    fields = ['composition', 'user', 'interpretation', 'public']
     
     
 
