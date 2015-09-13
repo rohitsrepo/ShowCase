51fbc4d85afb5febbccec988d2ef221484eacce6
rohit <rohitsrepo@gmail.com>
2015-09-14 01:56:16 +0530

    Introducing buckets to landing page

--- ShowCase/static/js/reader/reader.controller.js
+++ ShowCase/static/js/reader/reader.controller.js
@@ -1,63 +1,25 @@
 angular.module("ReaderApp")
 .controller("readerController", ["$scope",
- 'feedModel',
- 'analytics',
- '$location',
- function ($scope,
- 	feedModel,
- 	analytics,
- 	$location)
+    'bucketModel',
+    'bucketmodalService',
+    function ($scope, bucketModel, bucketmodalService)
  {
 	"use strict";
-	$scope.postsMeta = {pageVal: 1, disableGetMore: false, busy: false, next:'', previous:''};
-	$scope.posts = [];
 
-	function checkForPageValue() {
-		var page = $location.search()['page'];
-		page = parseInt(page);
+    $scope.readerBuckets = []
+    var buckets = ['social-satire', 'dance-of-light', 'the-dark-part']
 
-		if(!isNaN(page)){
-			$scope.postsMeta.pageVal = page;
-		}
-	};
-	checkForPageValue();
+    var getBuckets = function () {
+        angular.forEach(buckets, function (value) {
+            bucketModel.getBucket(value).then(function (response) {
+                $scope.readerBuckets.push(response);
+            });
+        });
+    }();
 
-	var getPosts = function () {
-
-		if (!$scope.postsMeta.disableGetMore) {
-			var pageVal = $scope.postsMeta.pageVal;
-			feedModel.getPosts(pageVal).then(function (posts) {
-				$scope.postsMeta.next = posts.next;
-				$scope.postsMeta.previous = posts.previous;
-
-				for (var i = 0; i < posts.results.length; i++) {
-			    	$scope.posts.push(posts.results[i]);
-			    }
-
-				if (posts.next == null){
-					$scope.postsMeta.disableGetMore = true;
-					analytics.logEvent('Reader', 'Load More Posts - Hit Bottom');
-				}
-
-				$scope.postsMeta.busy = false;
-			});
-		}
-
-		$scope.postsMeta.pageVal += 1;
-	}
-
-	$scope.loadMorePosts = function () {
-		if ($scope.postsMeta.busy) {
-			return;
-		}
-		
-		$scope.postsMeta.busy = true;
-		if ($scope.posts.length != 0){
-			analytics.logEvent('Reader', 'Load More Posts');
-		} else {
-			analytics.logEvent('Reader', 'Init');
-		}
-		getPosts();
-	};
+    $scope.showSeries = function (index) {
+        var bucket = $scope.readerBuckets[index];
+        bucketmodalService.showBucketArts(bucket);
+    };
 
 }]);
