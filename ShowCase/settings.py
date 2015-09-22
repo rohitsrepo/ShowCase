44ba42a37f04002287a9c8d30185196a4e62c821
rohit <rohitsrepo@gmail.com>
2015-09-22 10:29:14 +0530

    Fix middler ware settings

--- ShowCase/settings.py
+++ ShowCase/settings.py
@@ -96,7 +96,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    ' django.middleware.common.BrokenLinkEmailsMiddleware',
+    'django.middleware.common.BrokenLinkEmailsMiddleware',
 )
 
 ROOT_URLCONF = 'ShowCase.urls'
