4e4b29ea44abb7dd41c303a1d65dd6329b18b421
rohit <rohitsrepo@gmail.com>
2015-10-09 14:32:13 +0530

    Converting Arts to Fresh

--- ShowCase/static/js/explore/explore.controller.js
+++ ShowCase/static/js/explore/explore.controller.js
@@ -30,15 +30,7 @@ angular.module('ExploreApp')
 
     $scope.math = window.Math
 	$scope.arts = [];
-	$scope.postsMeta = {pageVal: 1, disableGetMore: false, busy: false, next:'', previous:'', feedType:'staff'};
-
-    var getPostFetcher = function () {
-        if ($scope.postsMeta.feedType === 'fresh') {
-            return feedModel.getFresh;
-        } else if ($scope.postsMeta.feedType === 'staff') {
-            return feedModel.getStaff;
-        }
-    };
+	$scope.postsMeta = {pageVal: 1, disableGetMore: false, busy: false, next:'', previous:''};
 
     // Disable scroll on parent page
     $rootScope.$on('$stateChangeSuccess',
@@ -57,7 +49,7 @@ angular.module('ExploreApp')
             progress.showProgress();
 
 	        var pageVal = $scope.postsMeta.pageVal;
-	        getPostFetcher()(pageVal).then(function (response) {
+	        feedModel.getFresh(pageVal).then(function (response) {
 	            $scope.postsMeta.next = response.next;
 	            $scope.postsMeta.previous = response.previous;
 
@@ -91,24 +83,6 @@ angular.module('ExploreApp')
 	}
 
 	$scope.loadMorePosts();
-
-    $scope.switchPostSource = function (feedType) {
-        if (feedType !== $scope.postsMeta.feedType) {
-            $scope.postsMeta.disableGetMore = true;
-
-            $scope.postsMeta = {
-                pageVal: 1,
-                busy: false,
-                next:'',
-                previous:'',
-                feedType:feedType
-            };
-
-            $scope.arts = [];
-            $scope.postsMeta.disableGetMore = false;
-            getPosts();
-        }
-    };
 
     $scope.handleBookMark = function (index) {
         var art = $scope.arts[index].content;
