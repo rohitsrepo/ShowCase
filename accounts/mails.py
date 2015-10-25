15579f38332f97bba0155c4982024fa91da69940
rohit <rohitsrepo@gmail.com>
2015-10-25 06:06:51 +0530

    Welcome mail for registration

--- accounts/mails.py
+++ accounts/mails.py
@@ -1,4 +1,5 @@
-from django.template.loader import render_to_string
+from django.template import Context
+from django.template.loader import render_to_string, get_template
 from django.core.mail import EmailMessage
 
 def send_warm_welcome(user):
@@ -8,3 +9,17 @@ def send_warm_welcome(user):
     message = render_to_string('mails/accounts/warm_welcome.txt')
 
     EmailMessage(subject, message, to=to, from_email=from_email).send(fail_silently=False)
+
+def send_welcome(user):
+    subject = "Welcome to ThirdDime"
+    to = [user.email]
+    from_email = 'ThirdDime Team <info@thirddime.com>'
+
+    ctx = {
+        'username': user.name,
+    }
+
+    message = get_template('mails/accounts/welcome.html').render(Context(ctx))
+    msg = EmailMessage(subject, message, to=to, from_email=from_email)
+    msg.content_subtype = 'html'
+    msg.send(fail_silently=False)
